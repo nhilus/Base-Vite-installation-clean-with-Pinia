6a6d4916e0f70cc940e6fcdeffba1134e391eb5f
António Barbosa <tobarbosa11@gmail.com>
2022-10-21 00:07:14 +0100

    exercicio 8 a funcionar, doing routing e css last

---
+++ base-clean/src/components/TaskDetail.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="task">
+        <input v-model = task.title type="text" :disabled="task.disabled">
+        <div class="icons">
+        <i 
+            class="material-symbols-outlined"
+            @click="task.disabled = !task.disabled"
+        >edit</i>
+        <i 
+            class="material-symbols-outlined"
+            :class="{disable:task.isComp}"
+            @click="taskStore.deleteTask(task.id)"
+        >delete</i>
+        <i 
+        class="material-symbols-outlined"
+        :class="{active:task.isComp}"
+        @click="taskStore.toggleComplete(task.id)"
+        >check_circle</i>
+        </div>
+    </div>
+</template>
+
+<script>
+import { useTaskStore } from '../stores/TaskStore';
+
+    export default {
+        props:['task'],
+
+        setup(){
+            const taskStore = useTaskStore()
+
+
+            return {taskStore}
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ base-clean/src/components/TaskDetails.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="task">
+        <input v-model = task.title type="text" :disabled="task.disabled">
+        <button>
+            <RouterLink to="/details" class="link">Detalhes</RouterLink>
+        </button>
+    </div>
+    <RouterView />
+</template>
+
+<script>
+import {RouterView, RouterLink} from 'vue-router'
+
+import { useTaskStore } from '../stores/TaskStore';
+
+    export default {
+        components: {
+            RouterLink,
+            RouterView,
+        },
+
+        props:['task'],
+
+        setup(){
+            const taskStore = useTaskStore()
+
+
+            return {taskStore}
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
